d117e62a28e2f0f68bbd8a7734b4ed213b080e0d
Ivana Seric <is28@njit.edu>
2026-02-11 22:14:47 -0500

    feat: the chart is actually 30-sec average to the 4th power

--- static/js/power_chart.js
+++ static/js/power_chart.js
@@ -3,8 +3,9 @@ import {get_data} from "./csv_data.js";
 
 async function powerChart(df) {
 
+    const exp = 2
     const yValue = d => d.power
-    const yValueSmooth = d => d.smooth_power
+    const yValueSmooth = d => d.smooth_power**exp
     const xValue = d => d.elapsed_time/60
 
     let dimensions = {
@@ -30,21 +31,21 @@ async function powerChart(df) {
         .style("transform",
                `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
 
-    const yScale = d3.scaleLog()
-        .clamp(true)
-        .domain([60, 600])
+    const yScale = d3.scaleLinear()
+        // .clamp(true)
+        .domain(d3.extent(df, yValueSmooth))
         .range([dimensions.boundedHeight, 0])
-        .base(3)
+        // .base(3)
         // .nice()
 
     const xScale = d3.scaleLinear()
         .domain(d3.extent(df, xValue))
-        .range([dimensions.boundedWidth, 0])
-//        .nice()
+        .range([0, dimensions.boundedWidth])
+       // .nice()
 
 
-    const thresholdWatts=244
-    const alpha = 0.3
+    const thresholdWatts=244**exp
+    const alpha = 0.5
     const zoneBoundaries = [0, 0.55*thresholdWatts, 0.75*thresholdWatts, 0.90*thresholdWatts, 1.05*thresholdWatts, 1.20*thresholdWatts, 1.50*thresholdWatts, yScale.domain()[1]]
     const zoneColors = ["#3db39f", "#3db33f", "#fcd549", "#fc9c49", "#e34074", "#8963d8", "#797388"]
 
@@ -144,13 +145,14 @@ async function powerChart(df) {
         .attr("fill", "none")
         // .attr("stroke",  )
        .attr("stroke", "url(#line-gradient)" )
-        .attr("stroke-width", 2)
+        .attr("stroke-width", 1)
 
 
     const yAxisGenerator = d3.axisLeft()
-        .scale(yScale).ticks(4)
+        .scale(yScale).ticks(10)
     const yAxis = bounds.append("g")
         .call(yAxisGenerator)
+        .attr("opacity", 0.5)
 
     const xAxisGenerator = d3.axisBottom()
         .scale(xScale)
@@ -159,7 +161,7 @@ async function powerChart(df) {
         .call(xAxisGenerator)
         .style("transform", `translateY(${
             dimensions.boundedHeight
-        }px)`)
+        }px)`).attr("opacity", 0.5)
 }
 const data_js = get_data();
 powerChart(data_js)
