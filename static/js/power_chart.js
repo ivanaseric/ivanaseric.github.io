eea8d8fea11e3a8c7a6e7499582c92317ae910a7
Ivana Seric <is28@njit.edu>
2026-02-25 22:57:40 -0500

    wip: working on the data viz

--- static/js/power_chart.js
+++ static/js/power_chart.js
@@ -1,6 +1,4 @@
-import {get_data} from "./csv_data.js";
-
-async function power_chart(container_id, {width, dataset} ) {
+export function power_chart(container_id, {width, dataset}) {
 
     let dimensions = {
         width: width,
@@ -25,46 +23,46 @@ async function power_chart(container_id, {width, dataset} ) {
 
     const bounds = svg.append("g")
         .style("transform",
-               `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
+            `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
 
     const exp = 1;
-    const yValue = d => d.smooth_power**exp;
-    const xValue = d => d.elapsed_time/60;
+    const yValue = d => d.smooth_power ** exp;
+    const xValue = d => d.elapsed_time / 60;
 
     const yScale = d3.scaleLinear()
         .domain([0, d3.extent(dataset, yValue)[1]])
         .range([dimensions.boundedHeight, 0])
-        // .nice()
+    // .nice()
 
     const xScale = d3.scaleLinear()
         .domain(d3.extent(dataset, xValue))
         .range([0, dimensions.boundedWidth])
 
 
-    const thresholdWatts = 244**exp;
+    const thresholdWatts = 244 ** exp;
     const zoneBoundaries = [
-        0, 0.55*thresholdWatts, 0.75*thresholdWatts, 0.90*thresholdWatts,
-        1.05*thresholdWatts, 1.20*thresholdWatts, 1.50*thresholdWatts,
+        0, 0.55 * thresholdWatts, 0.75 * thresholdWatts, 0.90 * thresholdWatts,
+        1.05 * thresholdWatts, 1.20 * thresholdWatts, 1.50 * thresholdWatts,
         yScale.domain()[1]];
     const zoneColors = [
         "#009e80", "#52c704", "#ffcb0e", "#ff7f0e",
         "#dd0447", "#6633cc", "#504861"];
 
     const zonesGradient = []
-    for (let i=0; i < zoneBoundaries.length-1; i++){
+    for (let i = 0; i < zoneBoundaries.length - 1; i++) {
 //        check that data actually contains the zone
-        if (zoneBoundaries[i] <= yScale.domain()[1]){
+        if (zoneBoundaries[i] <= yScale.domain()[1]) {
             // Add the "lower" boundary for the zone
             zonesGradient.push({
                 'offset': `${
-                    100-100*( yScale(zoneBoundaries[i]) / yScale.range()[0] )
+                    100 - 100 * (yScale(zoneBoundaries[i]) / yScale.range()[0])
                 }%`,
                 'color': zoneColors[i]
             });
             // Add the "upper" boundary for the zone
             zonesGradient.push({
                 'offset': `${
-                    100-100*( yScale(zoneBoundaries[i+1]) / yScale.range()[0] )
+                    100 - 100 * (yScale(zoneBoundaries[i + 1]) / yScale.range()[0])
                 }%`,
                 'color': zoneColors[i]
             });
@@ -82,9 +80,13 @@ async function power_chart(container_id, {width, dataset} ) {
         .selectAll("stop")
         .data(zonesGradient)
         .enter()
-            .append("stop")
-            .attr("offset", function(d) { return d.offset; })
-            .attr("stop-color", function(d) { return d.color; });
+        .append("stop")
+        .attr("offset", function (d) {
+            return d.offset;
+        })
+        .attr("stop-color", function (d) {
+            return d.color;
+        });
 
     const alpha = 0.3
     const areaOutline = d3.area()
@@ -105,7 +107,7 @@ async function power_chart(container_id, {width, dataset} ) {
         .data(dataset)
         .attr("d", lineGeneratorSmooth(dataset))
         .attr("fill", "none")
-        .attr("stroke", "url(#line-gradient)" )
+        .attr("stroke", "url(#line-gradient)")
         .attr("stroke-width", 1.5)
 
     // Add axes
@@ -130,8 +132,8 @@ async function power_chart(container_id, {width, dataset} ) {
     //  Add interactivity
     const y_tooltip = svg.append("text")
         .attr("id", "y-tooltip")
-        .attr("x", dimensions.margin.left/2)
-        .attr("y", dimensions.height/2 - dimensions.margin.top)
+        .attr("x", dimensions.margin.left / 2)
+        .attr("y", dimensions.height / 2 - dimensions.margin.top)
         .attr("font-size", "10px")
         .attr("fill", "teal")
         .attr("dominant-baseline", "middle")
@@ -152,7 +154,7 @@ async function power_chart(container_id, {width, dataset} ) {
 
     const bisect = d3.bisector(d => xValue(d)).center;
 
-    svg.on("mousemove", function(event) {
+    svg.on("mousemove", function (event) {
         const [xMouse, yMouse] = d3.pointer(event); // Get mouse position
         if (
             xMouse >= dimensions.margin.left && xMouse <= dimensions.width - dimensions.margin.right
@@ -188,16 +190,4 @@ async function power_chart(container_id, {width, dataset} ) {
     });
 }
 
-async function chartResize(){
-
-    const data_js = get_data();
-    var chartDiv = document.getElementById("wrapper");
-    var width = chartDiv.clientWidth;
-    console.log(width)
-    await power_chart("#wrapper",  {"width": width, dataset: data_js})
-}
-
-await chartResize();
-
-window.addEventListener('resize', chartResize );
 
