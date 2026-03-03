3ad262a5c9318195a3df397a1e909194e92b5956
Ivana Seric <is28@njit.edu>
2026-03-02 21:42:48 -0500

    feat: intermediate plots, y-axis label

--- static/js/power_chart_main.js
+++ static/js/power_chart_main.js
@@ -1,13 +1,17 @@
 import {get_data} from "./csv_data.js";
-import {power_chart} from "./power_chart.js"
+import {power_chart, power_chart_axis, power_chart_area, power_chart_line} from "./power_chart.js"
 
 async function chartResize() {
     const data_js = get_data();
     var chartDiv = document.getElementById("wrapper");
     var width = chartDiv.clientWidth;
     power_chart("#wrapper", {"width": width, dataset: data_js})
+    power_chart_axis("#chart_axis", {"width": width, dataset: data_js})
+    power_chart_area("#chart_area", {"width": width, dataset: data_js})
+    power_chart_line("#chart_line", {"width": width, dataset: data_js})
 }
 
 // first render
 await chartResize();
+
 window.addEventListener('resize', chartResize);
